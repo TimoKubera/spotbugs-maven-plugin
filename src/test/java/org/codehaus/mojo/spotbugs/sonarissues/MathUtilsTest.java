6a531753e2f0619ced7b658c3679a25390a50c10
TimoKubera <timokubera@yahoo.com>
2025-09-30 13:52:31 +0200

    🧪 Fix Mutation #10: org.codehaus.mojo.spotbugs.sonarissues.MathUtils.power

--- src/test/java/org/codehaus/mojo/spotbugs/sonarissues/MathUtilsTest.java
+++ src/test/java/org/codehaus/mojo/spotbugs/sonarissues/MathUtilsTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2005-2025 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the \"License\");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an \"AS IS\" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -64,6 +64,12 @@ public class MathUtilsTest {
         assertEquals(0, mathUtils.power(0, 3));
         assertEquals(1, mathUtils.power(0, 0));
         assertEquals(1, mathUtils.power(2, -3));
+        // Additional assertions for mutation coverage on negative bases
+        assertEquals(-8, mathUtils.power(-2, 3));
+        assertEquals(4, mathUtils.power(-2, 2));
+        // Trivial exponents
+        assertEquals(-2, mathUtils.power(-2, 1));
+        assertEquals(1, mathUtils.power(-2, -1));
     }
 
     @Test
